0becda2dd364c3e1b2c0265bca5cf75c162d594c
SUBHAM1509 <75567214+SUBHAM1509@users.noreply.github.com>
2021-05-31 17:02:50 +0530

    Add files via upload

---
+++ synchronousBallMovement-master/sketch.js
@@ -0,0 +1,31 @@
+var  database;
+var gameState=0
+var playerCount
+var backGroundImage
+var form,player,game
+var allPlayers
+
+
+function setup(){
+  database = firebase.database();
+
+  createCanvas(400,400);
+
+  game=new Game()
+  game.getState()
+  game.start()
+  
+ 
+}
+
+function draw(){
+ if(playerCount === 4) {
+   game.update(1)
+ }
+ if(gameState === 1) {
+   clear()
+   game.play()
+ }  
+
+}
+
